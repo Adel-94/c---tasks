8905d1b5ab54a4a66e33d27d84ba7efc22042e0f
Adel-94 <A.Ismayilova94@hotmail.com>
2017-05-04 18:21:39 +0400

    bbb

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int number;
+            Console.Write("Enter a Number : ");
+            number = int.Parse(Console.ReadLine());
+            if (number % 2 ==0)
+            {
+                Console.WriteLine(number * number);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine(number * number * number);
+                Console.ReadLine();
+            }
+    
+        }
+    }
+}
